55cf25aa7e6f291432cf4e8e8a39cf7cec847c5f
hjcreated <35471644+hjcreated@users.noreply.github.com>
2018-05-02 19:38:42 +0300

    Add files via upload

---
+++ testTool.java
@@ -0,0 +1,16 @@
+class testTool{
+public static void main (String [] args){
+int x = 0;
+int y = 0;
+if(X > Y){
+	System.out.println("X is Greater");
+	}//if
+else
+{
+	System.out.println("Y s Greater");
+	}//else
+
+
+
+}//main
+}//testTool
